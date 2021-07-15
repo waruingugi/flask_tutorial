19ed742b48affe84aa15b102db0b4c2f65183bed
waruingugi <waruingugientp@gmail.com>
2021-07-15 11:55:57 +0300

    Added pre-commit file

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+"""
+Simples Flask app code.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    """Returns a simple statement"""
     return "Hello World!"
